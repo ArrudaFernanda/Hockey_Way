0f87e861391e4ecb202912804059b80b63de740f
ArrudaFernanda <77357209+ArrudaFernanda@users.noreply.github.com>
2021-11-02 13:53:34 -0500

    Update collisionStick.gd

--- Scripts/collisionStick.gd
+++ Scripts/collisionStick.gd
@@ -3,6 +3,10 @@ extends CollisionShape2D
 onready var collisionNode = get_node(".")
 
 func _physics_process(delta):
+	#Quando solta o botão esquerdo do mouse ele (collisionStick)
+	#sobe 8 no eixo y. Se não, ele retorna a posição
+	#padrão em que ele fica eixo y. [Foi necessário ajuste
+	#nas configurações em Mapa de Entrada/ Todos os dispositivos]
 	if Input.is_action_just_released("release"):
 		collisionNode.position.y -= 8
 	else:
